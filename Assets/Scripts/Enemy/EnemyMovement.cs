2f810cf5cb0771ffc0801b45e234ee536bd8a6f0
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-09-09 15:46:46 +0300

    Enemy AI
    - Added EnemyMovement.cs
    - Added Enemy.prefab
    - Added LevelManager
    - Added LevelManager.cs

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("Refrences")]
+    [SerializeField] private Rigidbody2D _rb;
+
+    [Header("Attributes")]
+    [SerializeField] private float _moveSpeed = 2f;
+
+    private Transform _target;
+    private int _pathIndex = 0;
+
+    private void Start()
+    {
+        _target = LevelManager.main.path[_pathIndex];
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(_target.position, transform.position) <= .1f)
+        {
+            _pathIndex++;
+
+            if (_pathIndex == LevelManager.main.path.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                _target = LevelManager.main.path[_pathIndex];
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector2 direction = (_target.position - transform.position).normalized;
+
+        _rb.velocity = direction * _moveSpeed;
+    }
+}
