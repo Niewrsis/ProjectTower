63d36e7d251a77f6ec2a3d035daee8eb5b4ab4c7
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-09-19 12:22:02 +0300

    Level choosing
    + added 3 levels, working by reading PlayerPrefs.GetInt("level_index")

---
+++ Assets/_Source/SceneManagers/StorageLocationSceneManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StorageLocationSceneManager : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Button _level4Button;
+    [SerializeField] private Button _level5Button, _level6Button;
+
+    private void Start()
+    {
+        _level4Button.onClick.AddListener(onClickLevel4);
+        _level5Button.onClick.AddListener(onClickLevel5);
+        _level6Button.onClick.AddListener(onClickLevel6);
+    }
+    private void onClickLevel4()
+    {
+        if (PlayerPrefs.GetInt("level_index") >= 4)
+        {
+            SceneManager.LoadScene(9);
+        }
+    }
+    private void onClickLevel5()
+    {
+        if (PlayerPrefs.GetInt("level_index") >= 5)
+        {
+            SceneManager.LoadScene(10);
+        }
+    }
+    private void onClickLevel6()
+    {
+        if (PlayerPrefs.GetInt("level_index") >= 6)
+        {
+            SceneManager.LoadScene(11);
+        }
+    }
+}
